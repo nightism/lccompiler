45b75d79acff5e5f0f992780c55cc63124e659f8
myiking <smy961025@yeah.net>
2017-09-28 22:32:35 +0100

    Lexer updates 1. src/lexer/Tokeniser.java

--- src/lexer/Tokeniser.java
+++ src/lexer/Tokeniser.java
@@ -14,7 +14,7 @@ public class Tokeniser {
 
     private int error = 0;
     public int getErrorCount() {
-	return this.error;
+        return this.error;
     }
 
     public Tokeniser(Scanner scanner) {
@@ -23,7 +23,7 @@ public class Tokeniser {
 
     private void error(char c, int line, int col) {
         System.out.println("Lexing error: unrecognised character ("+c+") at "+line+":"+col);
-	error++;
+        error++;
     }
 
 
@@ -62,12 +62,17 @@ public class Tokeniser {
                 skipLine();
                 return next();
             } else if (scanner.peek() == '*') {
-                char nextOne = scanner.next();
-                while (!(nextOne == '*' && scanner.peek() == '/')) {
-                    nextOne = scanner.next();
+                try{
+                    char nextOne = scanner.next();
+                    while (!(nextOne == '*' && scanner.peek() == '/')) {
+                        nextOne = scanner.next();
+                    }
+                    scanner.next();
+                    return next();
+                } catch (EOFException eof) {
+                    error(c, line, column);
+                    return new Token(TokenClass.INVALID, line, column);
                 }
-                scanner.next();
-                return next();
             }
         }
 
@@ -84,6 +89,38 @@ public class Tokeniser {
             if (includeToken.equals("#include")) {
                 // TODO need to check whether the following is a valid string
                 return new Token(TokenClass.INCLUDE, line, thisCol);
+            }
+        }
+
+        /****** recognises the assignment operator and comparison operator ******/
+        if (c == '=') {
+            if (scanner.peek() == '=') {
+                scanner.next();
+                return new Token(TokenClass.EQ, line, column);
+            } else {
+                return new Token(TokenClass.EQ, line, column)
+            }
+        }
+
+        if (c == '!' && scanner.peek() == '=') {
+            return new Token(TokenClass.NE, line, column);
+        }
+
+        if (c == '<') {
+            if (scanner.peek() == '=') {
+                scanner.next();
+                return new Token(TokenClass.LE, line, column);
+            } else {
+                return new Token(TokenClass.LT, line, column);
+            }
+        }
+
+        if (c == '>') {
+            if (scanner.peek() == '=') {
+                scanner.next();
+                return new Token(TokenClass.GE, line, column);
+            } else {
+                return new Token(TokenClass.GT, line, column);
             }
         }
 
