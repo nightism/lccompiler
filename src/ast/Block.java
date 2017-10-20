de45ba3c2c706a1afe05c529934c4b7708eae689
myiking <smy961025@yeah.net>
2017-10-20 17:05:37 +0100

    AST updates nodes file updates

--- src/ast/Block.java
+++ src/ast/Block.java
@@ -1,10 +1,20 @@
 package ast;
 
+import java.util.List;
+
 public class Block extends Stmt {
 
     // to complete ...
+    public List<VarDecl> varDecls;
+    public List<Stmt> stmts;
+
+    public Block (List<VarDecl> varDecls, List<Stmt> stmts) {
+        this.varDecls = varDecls;
+        this.stmts = stmts;
+    }
 
     public <T> T accept(ASTVisitor<T> v) {
-	    return v.visitBlock(this);
+        return null;
+       // return v.visitBlock(this);
     }
 }
