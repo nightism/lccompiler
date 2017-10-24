9a63baf746488a4adddf3edbfb85fcf47d95d65f
myiking <smy961025@yeah.net>
2017-10-24 23:54:04 +0100

    Type Check 1. check var and fun declaration 2. check var expr

--- src/sem/TypeCheckVisitor.java
+++ src/sem/TypeCheckVisitor.java
@@ -12,28 +12,30 @@ public class TypeCheckVisitor extends BaseSemanticVisitor<Type> {
 
     @Override
     public Type visitBaseType(BaseType bt) {
-       // To be completed...
-       return null;
+        // To be completed...
+        return null;
     }
 
     @Override
     public Type visitStructTypeDecl(StructTypeDecl st) {
-       // To be completed...
-       return null;
+        // To be completed...
+        return null;
     }
 
     @Override
     public Type visitBlock(Block b) {
-       // To be completed...
-       return null;
+        // To be completed...
+        return null;
     }
 
     @Override
     public Type visitFunDecl(FunDecl p) {
-       // To be completed...
+        for (VarDecl vd : p.params) {
+            vd.accept(this);
+        }
+        p.block.accept(this);
        return null;
     }
-
 
     @Override
     public Type visitProgram(Program p) {
@@ -51,14 +53,16 @@ public class TypeCheckVisitor extends BaseSemanticVisitor<Type> {
 
     @Override
     public Type visitVarDecl(VarDecl vd) {
-       // To be completed...
+        if (vd.type == BaseType.VOID) {
+            error("the type of variable " + vd.varName + " cannot be VOID.");
+        }
        return null;
     }
 
     @Override
     public Type visitVarExpr(VarExpr v) {
-       // To be completed...
-       return null;
+       v.type = v.vd.type;
+       return v.type;
     }
 
     /** 
